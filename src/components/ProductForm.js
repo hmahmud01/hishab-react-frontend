27a634914651ddcd67628cb3e5ad4648dc53d8c5
hmahmud01 <hmahmud01@gmail.com>
2017-02-19 11:00:56 +0600

    working on product tables

--- src/components/ProductForm.js
+++ src/components/ProductForm.js
@@ -1,7 +1,70 @@
 import React, {Component} from  'react'
 import sample from '../pages/sound/sample.mp3';
+import $ from 'jquery';
+import Cookies from 'js-cookie';
 
 class ProductForm extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+
+        };
+        this.onProductAddClicked = this.onProductAddClicked.bind(this);
+    }
+
+    onProductAddClicked(event){
+        event.preventDefault();
+        //TODO create a tabletojson function to store the table products in json format
+
+        <td><a data-toggle="modal" className="btn btn-xs btn-info" href="#modal-product"><i className="fa fa-paste"></i> Edit </a> <button className="btn btn-xs btn-info" type="button"><i className="fa fa-times"></i> Delete </button></td>
+        var product = $('#product').find(":selected").text();
+        var qty = $('input[id="qty"]').val();
+        var unit = $('input[id="unit"]').val();
+        var price = $('input[id="price"]').val();
+        console.log(product);
+        $('tr:last').after("<tr><td>" + product + "</td><td>" +qty+ "</td><td>" +unit+ "</td><td>" +price+ "</td><td><a><i className=></i>new</a></td></tr>");
+        $('input[id="qty"]').val('');
+        $('input[id="unit"]').val('');
+        $('input[id="price"]').val('');
+    }
+
+    onNewProductAddClicked(event){
+        event.preventDefault();
+        //TODO create a tabletojson function to store the table products in json format
+        $.ajax({
+            method: 'post',
+            url: 'http://192.168.5.2:8000/api/v1/transcription/submit',
+            data: {
+                "buyer": document.getElementById("buyer").value,
+                "seller": document.getElementById("seller").value,
+                "products": [
+                    {
+                        "name": "alu",
+                        "qty": "5 kg",
+                        "price": 10.00,
+                        "total": 50.00
+                    }
+                ],
+                "total": document.getElementById("total").value,
+                "discount": document.getElementById("discount").value,
+                "paid": document.getElementById("paid").value,
+                "due": document.getElementById("due").value,
+                "uid": Cookies.get("uid"),
+                "tid": 1
+            },
+
+            success: function(response){
+                console.log(response);
+                var data = $.parseJSON(response);
+                this.setState({isError: false, message: data.msg, alertType: "success"});
+            }.bind(this),
+            error: function(response){
+                console.log(response.responseText);
+                var data = $.parseJSON(response.responseText);
+                this.setState({isError: true, message: data.msg, alertType: "danger"});
+            }.bind(this),
+        });
+    }
 
 	render(){
 		return (
@@ -83,7 +146,7 @@ class ProductForm extends Component {
                                     <div className="row">
                                         <div className="form-group"><label className="col-sm-4 control-label">Product name</label>
                                             <div className="col-sm-6">
-                                                <select className="form-control m-b" name="product">
+                                                <select className="form-control m-b" name="product" id="product">
                                                     <option>alu</option>
                                                     <option>potol</option>
                                                     <option>am</option>
@@ -96,24 +159,24 @@ class ProductForm extends Component {
                                         </div>
                                         <div className="form-group"><label className="col-sm-4 control-label">Quantity</label>
                                             <div className="col-sm-8">
-                                                <input type="text" placeholder="Quantity" className="form-control" />
+                                                <input id="qty" type="text" placeholder="Quantity" className="form-control" />
                                             </div>
                                         </div>
                                         <div className="form-group"><label className="col-sm-4 control-label">Unit</label>
                                             <div className="col-sm-8">
-                                                <input type="text" placeholder="Unit" className="form-control" />
+                                                <input id="unit" type="text" placeholder="Unit" className="form-control" />
                                             </div>
                                         </div>
                                         <div className="form-group"><label className="col-sm-4 control-label">Price</label>
                                             <div className="col-sm-8">
-                                                <input type="text" placeholder="Price" className="form-control" />                                               
+                                                <input id="price" type="text" placeholder="Price" className="form-control" />                                               
                                             </div>
                                         </div>                                        
                                     </div>
                                 </div>
                                 <div className="modal-footer">
                                     <button type="button" className="btn btn-default" data-dismiss="modal">Close</button>
-                                    <button type="button" className="btn btn-primary">Save changes</button>
+                                    <button type="button" className="btn btn-primary" data-dismiss="modal" onClick={this.onProductAddClicked}>Save changes</button>
                                 </div>
                             </form>
                         </div>
