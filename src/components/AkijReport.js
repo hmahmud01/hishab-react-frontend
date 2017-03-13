85a4f4ff29061baaba92933b2b31b3dc178e66b8
hmahmud01 <hmahmud01@gmail.com>
2017-03-13 12:39:22 +0600

    akij report working with tested json data

--- src/components/AkijReport.js
+++ src/components/AkijReport.js
@@ -5,21 +5,39 @@ class AkijReport extends Component {
         super(props);
         this.state = {
             productList : ["Marlboro", "Winston", "Sheikh"], 
-            dataList : [{sr: "Abul", route: "Gulshan", sales: [{buy: 30, sell: 15, ret: 10},
-            													{buy: 15, sell: 20, ret: 22},
-            													{buy: 25, sell: 45, ret: 60}]},
-                       {sr: "Kashem", route: "Dhanmondi", sales: [{buy: 14, sell: 15, ret: 10},
-            													{buy: 115, sell: 20, ret: 22},
-            													{buy: 225, sell: 45, ret: 60}]},
-                       {sr: "Rashid", route: "Bonani", sales: [{buy: 2230, sell: 15, ret: 10},
-            													{buy: 415, sell: 20, ret: 22},
-            													{buy: 525, sell: 45, ret: 60}]}]
+            dataList : [{
+                        sr: "Abul", 
+                        route: "Gulshan",
+                        products: ["Marlboro", "Winston", "Sheikh"] ,
+                        sales : [["30","15","10"],["15","20","22"],["25","45","60"]]
+                        },
+                       {
+                        sr: "Kashem", route: "Dhanmondi", 
+                        products: ["Marlboro", "Winston", "Sheikh"] ,
+                        sales: [["30","15","10"],["15","20","22"],["25","45","60"]]
+                        },
+                       {
+                        sr: "Rashid", route: "Bonani", 
+                        products: ["Marlboro", "Winston", "Sheikh"] ,
+                        sales: [["30","15","10"],["15","20","22"],["25","45","60"]]
+                        }]
         };
     }
     
-//    dataList : [[30,15,10],[15,20,22],[25,45,60],
-//                       [30,15,10],[15,20,22],[25,45,60],
-//                       [30,15,10],[15,20,22],[25,45,60]]
+   // sales : [[30,15,10],[15,20,22],[25,45,60],
+   //                    [30,15,10],[15,20,22],[25,45,60],
+   //                    [30,15,10],[15,20,22],[25,45,60]]
+
+
+// dataList : [{sr: "Abul", route: "Gulshan", sales: [{buy: 30, sell: 15, ret: 10},
+//                                                                 {buy: 15, sell: 20, ret: 22},
+//                                                                 {buy: 25, sell: 45, ret: 60}]},
+//                        {sr: "Kashem", route: "Dhanmondi", sales: [{buy: 14, sell: 15, ret: 10},
+//                                                                 {buy: 115, sell: 20, ret: 22},
+//                                                                 {buy: 225, sell: 45, ret: 60}]},
+//                        {sr: "Rashid", route: "Bonani", sales: [{buy: 2230, sell: 15, ret: 10},
+//                                                                 {buy: 415, sell: 20, ret: 22},
+//                                                                 {buy: 525, sell: 45, ret: 60}]}]
     
 
 	render(){
@@ -28,12 +46,17 @@ class AkijReport extends Component {
             textAlign: "center",
             border: "1px solid"
         };
+
+        var productDataList = this.state.dataList.map(function(pData, index){
+            return pData.products
+        });
         
         var productHeaders = this.state.productList.map(function(product, index){
             return(
                 <th key={index} colSpan="3" style={headerDesign}>{product}</th>
             );
         });
+        
         
         var productSubHeaders = this.state.productList.map(function(product, index){
             var subHeading = ["Buy", "Sell", "Return"];
@@ -53,11 +76,16 @@ class AkijReport extends Component {
             	    <td >{data.sr}</td>
             	    <td >{data.route}</td>
                     {data.sales.map(function(individualData,index){
-                    	return (
-                			<td key={index}>{individualData.buy} {individualData.sell} {individualData.ret}</td>
+                        // var ret = individualData.buy - individualData.sell
+                    	return individualData.map(function(cell, di){
+                            return(
+                                <th key={di}>{cell}</th>
+                            )
+                        });
+                			// <td key={index}>{individualData}</td>
                 			// <td key={index}>{individualData.buy}</td>
                 			// <td key={index}>{individualData.buy}</td>
-                    	)
+                    	
                     })
                 	}
                 </tr>
@@ -87,18 +115,6 @@ class AkijReport extends Component {
 							  	</thead>
 							  	<tbody>
 								  	{dataRows}
-								  	<tr>
-									    <td>Kashen</td>
-									    <td>Dhanmondi</td>
-            
-									    <td>50 stick</td>
-									    <td>20 Stick</td>
-									    <td>30 stick</td>
-            
-									    <td>10 stick</td>
-									    <td>05 Stick</td>
-									    <td>05 stick</td>
-								  	</tr>
 							  	</tbody>	
 							</table>
 				    	</div>
