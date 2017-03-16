c2a84a3924168ccac402b7b457c899460bfff69f
hmahmud01 <hmahmud01@gmail.com>
2017-03-16 14:05:04 +0600

    csv working, table need to reconstruct, maybe new json needed

--- src/components/AkijReport.js
+++ src/components/AkijReport.js
@@ -4,6 +4,7 @@ import Cookies from 'js-cookie';
 
 import Json from '../utils/Json';
 import Ajax from '../utils/Ajax';
+import Logger from '../utils/Logger';
 
 class AkijReport extends Component {
     constructor(props){
@@ -12,6 +13,9 @@ class AkijReport extends Component {
             productList : [], 
             dataList : []
         };
+        this.download_csv = this.download_csv.bind(this);
+        this.export_table_to_csv = this.export_table_to_csv.bind(this);
+        this.tableDownload = this.tableDownload.bind(this);
     }
 
 
@@ -33,6 +37,68 @@ class AkijReport extends Component {
         ajax.getData('reports/sr', params);
 
     }
+
+    //export csv is currently in deploy
+
+    download_csv(csv, filename){
+        var csvFile;
+        var downloadLink;
+
+        csvFile = new Blob([csv], {type: "text/csv"});
+
+        downloadLink = document.createElement("a");
+
+        downloadLink.download = filename;
+
+        downloadLink.href = window.URL.createObjectURL(csvFile);
+
+        downloadLink.style.display = "none";
+
+        document.body.appendChild(downloadLink);
+
+        downloadLink.click();
+        console.log("end of download csv");
+    }
+
+    export_table_to_csv(filename){
+        var csv = [];
+        var rows = document.querySelectorAll("table tr");
+        // console.log("inside row");
+        // console.log(rows);
+        for(var i=0; i<rows.length; i++){
+            var row = [];
+            console.log("inside col_0");            
+            var col_0 = rows[0];
+            console.log(col_0);
+            console.log("inside col_1");            
+            var col_1 = rows[1];
+            console.log(col_1);
+
+            var _th = "<th><th>";
+            col_1 = _th + _th + col_1;
+            // col_1.unshift(_th);
+            console.log(col_1[0]);
+
+            var cols = rows[i].querySelectorAll("td, th");
+            // console.log("inside col");
+            // console.log(cols);
+
+            for (var j=0; j<cols.length; j++)
+                row.push(cols[j].innerText);
+
+            csv.push(row.join(","));
+        }
+        // console.log("in table to csv before download");
+        this.download_csv(csv.join("\n"), filename);
+        // console.log("in table to csv after download");
+    }
+
+    tableDownload(){
+        this.export_table_to_csv("table.csv");
+        // console.log("inside table download function");
+    }
+
+
     
 
 	render(){
@@ -93,7 +159,7 @@ class AkijReport extends Component {
 			    <div className="col-lg-12">
 			        <div className="ibox float-e-margins">
 			            <div className="ibox-title">
-			                <h5>Akij Reports</h5>
+			                <h5>Akij Reports <button className="btn btn-xs btn-primary" onClick={this.tableDownload}>Export to CSV</button></h5>
 			            </div>
 				        <div className="ibox-content" style={tableDesign}>
 					        <table id="akij-report" className="table table-striped table-bordered table-hover">
