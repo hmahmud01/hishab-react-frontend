cc0f286460cc1078fd4894e6b9aad6220ec7d0c1
hmahmud01 <hmahmud01@gmail.com>
2017-03-18 13:27:44 +0600

    form refactoring

--- src/components/ProductForm.js
+++ src/components/ProductForm.js
@@ -141,54 +141,6 @@ class ProductForm extends Component {
         
         var ajax = new Ajax(callback);
         ajax.getData('transaction/product/attribute', params);
-
-        // $.ajax({
-        //     method: 'get',
-        //     url: 'transaction/product/attribute',
-        //     data: {
-        //         "uid" : Cookies.get("uid"),
-        //         "pid" : id
-        //     },
-        //     success: function(response){
-        //         var data = $.parseJSON(response);
-        //         var output = [];
-        //         var headers = [];
-        //         headers[0] = "Product Name";
-        //         headers[1] = "Category";
-        //         headers[2] = "Unit Price";
-        //         headers[3] = "Unit";
-        //         headers[4] = "Quantity";
-
-        //         output[0] = {header: "Product Name", data: data.product_name};
-        //         output[1] = {header: "Category", data: data.product_category};
-        //         output[2] = {header: "Unit Price", data: data.product_unit_price};
-        //         output[3] = {header: "Unit", data: data.product_unit};
-        //         output[4] = {header: "Quantity", data: data.product_quantity};
-
-                
-        //          for (var i=5; i< data.product_attribute.length+5; i++){
-        //              output[i] = {header: data.product_attribute[i-5], data: ""};
-        //              headers[i] = data.product_attribute[i-5];
-        //          }
-
-        //         var modalFields = output.map(
-        //             function (product, index){
-        //                 var id = "item"+index;
-        //                 var head = product.header;
-        //                 return(
-        //                     <TextInput key={index} id={id} label={product.header} placeholder={product.header} value={product.data}/>
-        //                 );
-        //             }
-        //         );
-                
-        //         var headerArray = this.refs.data.state.headers.slice();
-        //         headerArray.push(headers);
-        //         this.setState({headers: headers, modalFields: modalFields});
-        //     }.bind(this),
-        //     error: function(response){
-                
-        //     }
-        // });
     }
     
     categorySelected(id){
